a5b77208c01c17e6473035f75727e20248ab4f17
avraneel <avraneelpal22@gmail.com>
2022-12-26 12:25:23 +0530

    Linking article names with detail

--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, re_path
 from . import views
 
+app_name='articles'
+
 urlpatterns = [
-    path('', views.article_list),
-    path('<slug:slug>', views.article_detail)
+    path('', views.article_list, name='list'),
+    path('<slug:slug>', views.article_detail, name='detail')
 ]
