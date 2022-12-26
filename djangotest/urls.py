6fa49c16000d0d5328c2ae984640e644375e6d23
avraneel <avraneelpal22@gmail.com>
2022-12-26 11:12:22 +0530

    Added assets and static dirs in settings.py

--- djangotest/urls.py
+++ djangotest/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path, re_path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +24,5 @@ urlpatterns = [
     re_path(r'^$', views.homepage),
     path('articles/', include('articles.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
